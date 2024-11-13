118c3eac7086af7546ad859d7c8f9c766a26576c
bmitegui <bmite@hubilogist.com>
2024-11-13 10:12:07 -0500

    BM: Limpieza codigo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 // Grid version 2
 import Grid from '@mui/material/Grid2'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Grid container spacing={5}>
