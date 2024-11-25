f19a5b62a2b629f692abc95d78ae711a85418390
bmitegui <bmite@hubilogist.com>
2024-11-25 09:43:36 -0500

    Componente IndicatorWeather: Componente MUI Paper y Typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import './App.css'
 // Grid version 2
 import Grid from '@mui/material/Grid2'
+import IndicatorWeather from './components/IndicatorWeather';
+
 
 function App() {
-
   return (
     <Grid container spacing={5}>
+      <Grid size={{ xs: 12, xl: 3 }}> <IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} />
+      </Grid>
+      <Grid size={{ xs: 12, xl: 3 }}> <IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"} />
+      </Grid>
+      <Grid size={{ xs: 12, xl: 3 }}> <IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} />
+      </Grid>
+      <Grid size={{ xs: 12, xl: 3 }}> <IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} />
+      </Grid>
 
-        {/* Indicadores */}
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 1</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 2</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 3</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 4</Grid>
+      {/* Tabla */}
+      <Grid size={{ xs: 12, xl: 8 }}>Elemento: Tabla</Grid>
 
-        {/* Tabla */}
-        <Grid size={{ xs: 12, xl: 8 }}>Elemento: Tabla</Grid>
-
-        {/* Gráfico */}
-        <Grid size={{ xs: 12, xl: 4 }}>Elemento: Gráfico 1</Grid>
-       
+      {/* Gráfico */}
+      <Grid size={{ xs: 12, xl: 4 }}>Elemento: Gráfico 1</Grid>
     </Grid>
-)
+  )
 }
 
 export default App
